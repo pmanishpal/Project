4e51915ff387d7a9807825d5e9d15c4f18043bc1
pmanishpal <pmanishpal@gmail.com>
2026-06-13 13:08:23 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
-def add(a, b):
-    return a + b
+def add(a, b, c):
+    return a + b +c
 
-def subtract(a, b):
-    return a - b
+def subtract(a, b,e):
+    return a - b  e
 
 def divide(a, b):
     return a / b
